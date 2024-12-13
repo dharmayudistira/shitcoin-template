5dfa2c818fd0769a20d41b158807ea074ed39608
Dharma Yudistira <dharmayudistira2000@gmail.com>
2024-12-13 22:12:54 +0700

    add api chat

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,41 @@
+import { customModel } from "@/app/chatbot/_ai";
+import { models } from "@/app/chatbot/_ai/models";
+import { regularPrompt } from "@/app/chatbot/_ai/prompt";
+import type { Message } from "ai";
+import { convertToCoreMessages, StreamData, streamText } from "ai";
+
+
+export const maxDuration = 60;
+
+export async function POST(request: Request) {
+    const { messages, modelId }: { messages: Message[]; modelId: string } =
+        await request.json();
+
+    const model = models.find((model) => model.id === modelId);
+
+    if (!model) {
+        return new Response("Model not found", { status: 404 });
+    }
+
+    const coreMessages = convertToCoreMessages(messages);
+    const streamingData = new StreamData();
+
+    const result = await streamText({
+        model: customModel("gpt-4o-mini"),
+        system: regularPrompt,
+        messages: coreMessages,
+        maxSteps: 5,
+
+        onFinish: async () => {
+            streamingData.close();
+        },
+        experimental_telemetry: {
+            isEnabled: true,
+            functionId: "stream-text",
+        },
+    });
+
+    return result.toDataStreamResponse({
+        data: streamingData,
+    });
+}
